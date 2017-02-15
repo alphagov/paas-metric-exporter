319fd472acfde22fa6a233c78aa678b32c730861
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2017-02-15 11:39:45 +0000

    Replace the uaago with go-cfclient
    We'd like to support a wider range of CF configuration. The uaago
    library has been somewhat limited in a way of providing the
    authentication as it has been limiting the `grant_type` to
    `client_credential`.
    
    We have found the need to implement the authentication based on the
    username and password instead, to provide our tenants with the ability
    to manually deploy the nozzle for themselves.
    
    We no longer require the doppler url as we obtain the API endpoint
    from the new client library.

--- main.go
+++ main.go
@@ -8,45 +8,60 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/cloudfoundry-community/go-cfclient"
 	"github.com/cloudfoundry/noaa"
 	"github.com/cloudfoundry/noaa/events"
 	"github.com/pivotal-cf/graphite-nozzle/metrics"
 	"github.com/pivotal-cf/graphite-nozzle/processors"
-	"github.com/pivotal-cf/graphite-nozzle/token"
 	"github.com/quipo/statsd"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	dopplerEndpoint   = kingpin.Flag("doppler-endpoint", "Doppler endpoint").Default("wss://doppler.10.244.0.34.xip.io:443").OverrideDefaultFromEnvar("DOPPLER_ENDPOINT").String()
-	uaaEndpoint       = kingpin.Flag("uaa-endpoint", "UAA endpoint").Default("https://uaa.10.244.0.34.xip.io").OverrideDefaultFromEnvar("UAA_ENDPOINT").String()
+	apiEndpoint       = kingpin.Flag("api-endpoint", "API endpoint").Default("https://api.10.244.0.34.xip.io").OverrideDefaultFromEnvar("API_ENDPOINT").String()
 	subscriptionId    = kingpin.Flag("subscription-id", "Id for the subscription.").Default("firehose").OverrideDefaultFromEnvar("SUBSCRIPTION_ID").String()
 	statsdEndpoint    = kingpin.Flag("statsd-endpoint", "Statsd endpoint").Default("10.244.11.2:8125").OverrideDefaultFromEnvar("STATSD_ENDPOINT").String()
 	statsdPrefix      = kingpin.Flag("statsd-prefix", "Statsd prefix").Default("mycf.").OverrideDefaultFromEnvar("STATSD_PREFIX").String()
 	prefixJob         = kingpin.Flag("prefix-job", "Prefix metric names with job.index").Default("false").OverrideDefaultFromEnvar("PREFIX_JOB").Bool()
-	username          = kingpin.Flag("username", "Firehose username.").Default("admin").OverrideDefaultFromEnvar("FIREHOSE_USERNAME").String()
-	password          = kingpin.Flag("password", "Firehose password.").Default("admin").OverrideDefaultFromEnvar("FIREHOSE_PASSWORD").String()
+	username          = kingpin.Flag("username", "UAA username.").Default("").OverrideDefaultFromEnvar("USERNAME").String()
+	password          = kingpin.Flag("password", "UAA password.").Default("").OverrideDefaultFromEnvar("PASSWORD").String()
+	clientID          = kingpin.Flag("client-id", "Client ID.").Default("").OverrideDefaultFromEnvar("CLIENT_ID").String()
+	clientSecret      = kingpin.Flag("client-secret", "Client Secret.").Default("").OverrideDefaultFromEnvar("CLIENT_SECRET").String()
 	skipSSLValidation = kingpin.Flag("skip-ssl-validation", "Please don't").Default("false").OverrideDefaultFromEnvar("SKIP_SSL_VALIDATION").Bool()
 	debug             = kingpin.Flag("debug", "Enable debug mode. This disables forwarding to statsd and prints to stdout").Default("false").OverrideDefaultFromEnvar("DEBUG").Bool()
+	appGUID           = kingpin.Flag("app-guid", "app GUID to stream events from").Default("").OverrideDefaultFromEnvar("APP_GUID").String()
 )
 
 func main() {
+	var (
+		authToken string
+		err       error
+	)
+
 	kingpin.Parse()
 
-	tokenFetcher := &token.UAATokenFetcher{
-		UaaUrl:                *uaaEndpoint,
-		Username:              *username,
-		Password:              *password,
-		InsecureSSLSkipVerify: *skipSSLValidation,
+	c := &cfclient.Config{
+		ApiAddress:        *apiEndpoint,
+		SkipSslValidation: *skipSSLValidation,
+		Username:          *username,
+		Password:          *password,
+		ClientID:          *clientID,
+		ClientSecret:      *clientSecret,
 	}
 
-	authToken, err := tokenFetcher.FetchAuthToken()
+	client, err := cfclient.NewClient(c)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
 
-	consumer := noaa.NewConsumer(*dopplerEndpoint, &tls.Config{InsecureSkipVerify: *skipSSLValidation}, nil)
+	authToken, err = client.GetToken()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	consumer := noaa.NewConsumer(client.Endpoint.DopplerEndpoint, &tls.Config{InsecureSkipVerify: *skipSSLValidation}, nil)
 
 	httpStartStopProcessor := processors.NewHttpStartStopProcessor()
 	valueMetricProcessor := processors.NewValueMetricProcessor()
@@ -64,7 +79,11 @@ func main() {
 	go func() {
 		defer close(msgChan)
 		errorChan := make(chan error)
-		go consumer.Firehose(*subscriptionId, authToken, msgChan, errorChan, nil)
+		if *appGUID == "" {
+			go consumer.Firehose(*subscriptionId, authToken, msgChan, errorChan, nil)
+		} else {
+			go consumer.Stream(*appGUID, authToken, msgChan, errorChan, nil)
+		}
 
 		for err := range errorChan {
 			fmt.Fprintf(os.Stderr, "%v\n", err.Error())
