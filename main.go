ac1499ff75d19e553f87d19c705dbee59b0134bc
bandesz <bandesz@gmail.com>
2017-03-30 14:06:53 +0100

    Process only container metrics from the event stream

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/alphagov/paas-cf-apps-statsd/processors"
 	"github.com/cloudfoundry-community/go-cfclient"
 	"github.com/cloudfoundry/noaa/consumer"
+	"github.com/cloudfoundry/sonde-go/events"
 	"github.com/quipo/statsd"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
@@ -145,7 +146,9 @@ func (m *metricProcessor) updateApps() error {
 			go func(currentApp cfclient.App) {
 				for message := range msg {
 					stream := metrics.Stream{Msg: message, App: currentApp, Tmpl: *metricTemplate}
-					m.msgChan <- &stream
+					if (*message.EventType == events.Envelope_ContainerMetric) {
+						m.msgChan <- &stream
+					}
 				}
 			}(app)
 
