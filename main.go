555ba9bc4c0426d6eebcb539c660b48090b9d3d5
Michael Mokrysz <michael.mokrysz@digital.cabinet-office.gov.uk>
2017-12-05 16:51:09 +0000

    Refactor main package into separate files

--- main.go
+++ main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
-	"net/url"
 	"os"
-	"strings"
-	"sync"
-	"time"
 
 	"github.com/alphagov/paas-cf-apps-statsd/metrics"
 	"github.com/alphagov/paas-cf-apps-statsd/processors"
 	"github.com/cloudfoundry-community/go-cfclient"
-	"github.com/cloudfoundry/noaa/consumer"
-	"github.com/cloudfoundry/sonde-go/events"
 	"github.com/quipo/statsd"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
@@ -29,30 +22,6 @@ var (
 	updateFrequency   = kingpin.Flag("update-frequency", "The time in seconds, that takes between each apps update call.").Default("300").OverrideDefaultFromEnvar("UPDATE_FREQUENCY").Int64()
 	metricTemplate    = kingpin.Flag("metric-template", "The template that will form a new metric namespace.").Default("{{.Space}}.{{.App}}.{{.Instance}}.{{.Metric}}").OverrideDefaultFromEnvar("METRIC_TEMPLATE").String()
 )
-
-type metricProcessor struct {
-	cfClient       *cfclient.Client
-	cfClientConfig *cfclient.Config
-	msgChan        chan *metrics.Stream
-	errorChan      chan error
-	watchedApps    map[string]chan cfclient.App
-	sync.RWMutex
-}
-
-func (m *metricProcessor) RefreshAuthToken() (token string, authError error) {
-	token, err := m.cfClient.GetToken()
-	if err != nil {
-		err := m.authenticate()
-
-		if err != nil {
-			return "", err
-		}
-
-		return m.cfClient.GetToken()
-	}
-
-	return token, nil
-}
 
 func main() {
 	kingpin.Parse()
@@ -114,153 +83,5 @@ func main() {
 			}
 		}
 		processedMetrics = nil
-	}
-}
-
-func (m *metricProcessor) authenticate() (err error) {
-	client, err := cfclient.NewClient(m.cfClientConfig)
-	if err != nil {
-		return err
-	}
-
-	m.cfClient = client
-	return nil
-}
-
-func updateAppSpaceData(app *cfclient.App) error {
-	if app.SpaceData == (cfclient.SpaceResource{}) {
-		space, err := app.Space()
-		if err != nil {
-			return err
-		}
-		org, err := space.Org()
-		if err != nil {
-			return err
-		}
-		space.OrgData.Entity = org
-		app.SpaceData.Entity = space
-	}
-	return nil
-}
-
-func (m *metricProcessor) startStream(app cfclient.App) chan cfclient.App {
-	appChan := make(chan cfclient.App)
-	go func() {
-		conn := consumer.New(m.cfClient.Endpoint.DopplerEndpoint, &tls.Config{InsecureSkipVerify: *skipSSLValidation}, nil)
-		conn.RefreshTokenFrom(m)
-		defer func() {
-			m.Lock()
-			defer m.Unlock()
-			delete(m.watchedApps, app.Guid)
-		}()
-
-		authToken, err := m.cfClient.GetToken()
-		if err != nil {
-			m.errorChan <- err
-			return
-		}
-		msgs, errs := conn.Stream(app.Guid, authToken)
-		for {
-			select {
-			case message, ok := <-msgs:
-				if !ok {
-					return
-				}
-				stream := metrics.Stream{Msg: message, App: app, Tmpl: *metricTemplate}
-				if *message.EventType == events.Envelope_ContainerMetric {
-					m.msgChan <- &stream
-				}
-			case err, ok := <-errs:
-				if !ok {
-					errs = nil
-					continue
-				}
-				if err == nil {
-					continue
-				}
-				m.errorChan <- err
-			case updatedApp, ok := <-appChan:
-				if !ok {
-					appChan = nil
-					conn.Close()
-					continue
-				}
-				app = updatedApp
-			}
-		}
-	}()
-	return appChan
-}
-
-func (m *metricProcessor) getApps() ([]cfclient.App, error) {
-	q := url.Values{}
-	apps, err := m.cfClient.ListAppsByQuery(q)
-	if err != nil {
-		return nil, err
-	}
-	for _, app := range apps {
-		if err = updateAppSpaceData(&app); err != nil {
-			return nil, err
-		}
-	}
-	return apps, nil
-}
-
-func (m *metricProcessor) isWatched(guid string) bool {
-	m.RLock()
-	defer m.RUnlock()
-	_, exists := m.watchedApps[guid]
-	return exists
-}
-
-func (m *metricProcessor) updateApps() error {
-
-	m.Lock()
-	defer m.Unlock()
-
-	apps, err := m.getApps()
-	if err != nil {
-		return err
-	}
-
-	running := map[string]bool{}
-	for _, app := range apps {
-		running[app.Guid] = true
-		if appChan, ok := m.watchedApps[app.Guid]; ok {
-			appChan <- app
-		} else {
-			appChan = m.startStream(app)
-			m.watchedApps[app.Guid] = appChan
-		}
-	}
-
-	for appGuid, appChan := range m.watchedApps {
-		if ok := running[appGuid]; !ok {
-			close(appChan)
-		}
-	}
-
-	return nil
-}
-
-func (m *metricProcessor) process(updateFrequency int64) error {
-
-	for {
-		err := m.authenticate()
-		if err != nil {
-			return err
-		}
-
-		for {
-			err := m.updateApps()
-			if err != nil {
-				if strings.Contains(err.Error(), `"error":"invalid_token"`) {
-					break
-				}
-				return err
-			}
-
-			time.Sleep(time.Duration(updateFrequency) * time.Second)
-		}
 	}
 }
--- metric_processor_test.go
+++ metric_processor_test.go
@@ -7,13 +7,10 @@ import (
 	"net/http"
 	"net/url"
 	"regexp"
-	"sync"
 	"time"
 
 	"github.com/alphagov/paas-cf-apps-statsd/metrics"
 	"github.com/cloudfoundry-community/go-cfclient"
-	"github.com/cloudfoundry/sonde-go/events"
-	"github.com/gogo/protobuf/proto"
 	"github.com/gorilla/websocket"
 	"github.com/onsi/gomega/ghttp"
 	"golang.org/x/oauth2"
@@ -24,7 +21,7 @@ type TokenErr struct {
 	Description string `json:"error_description"`
 }
 
-var _ = Describe("Main", func() {
+var _ = Describe("metricProcessor", func() {
 	var (
 		apiServer  *ghttp.Server
 		tcServer   *ghttp.Server
@@ -428,157 +425,3 @@ var _ = Describe("Main", func() {
 		})
 	})
 })
-
-var _ = Describe("test helpers", func() {
-	Describe("testAppResponse", func() {
-		var apps []cfclient.App
-
-		Context("no apps", func() {
-			BeforeEach(func() {
-				apps = []cfclient.App{}
-			})
-
-			It("should return a single page with no apps", func() {
-				Expect(
-					testAppResponse(apps),
-				).To(
-					Equal(cfclient.AppResponse{
-						Count:     0,
-						Pages:     1,
-						Resources: []cfclient.AppResource{},
-					}),
-				)
-			})
-		})
-
-		Context("three apps", func() {
-			BeforeEach(func() {
-				apps = []cfclient.App{
-					{Guid: "11111111-1111-1111-1111-111111111111"},
-					{Guid: "22222222-2222-2222-2222-222222222222"},
-					{Guid: "33333333-3333-3333-3333-333333333333"},
-				}
-			})
-
-			It("should return a single page with three apps", func() {
-				Expect(
-					testAppResponse(apps),
-				).To(
-					Equal(cfclient.AppResponse{
-						Count: 3,
-						Pages: 1,
-						Resources: []cfclient.AppResource{
-							{
-								Meta:   cfclient.Meta{Guid: apps[0].Guid},
-								Entity: apps[0],
-							}, {
-								Meta:   cfclient.Meta{Guid: apps[1].Guid},
-								Entity: apps[1],
-							}, {
-								Meta:   cfclient.Meta{Guid: apps[2].Guid},
-								Entity: apps[2],
-							},
-						},
-					}),
-				)
-			})
-		})
-	})
-})
-
-func testAppResponse(apps []cfclient.App) cfclient.AppResponse {
-	resp := cfclient.AppResponse{
-		Count:     len(apps),
-		Pages:     1,
-		Resources: make([]cfclient.AppResource, len(apps)),
-	}
-
-	for i, app := range apps {
-		resp.Resources[i] = cfclient.AppResource{
-			Meta:   cfclient.Meta{Guid: app.Guid},
-			Entity: app,
-		}
-	}
-
-	return resp
-}
-
-func testSpaceResource(spaceGuid, orgGuid string) cfclient.SpaceResource {
-	return cfclient.SpaceResource{
-		Meta:   cfclient.Meta{Guid: spaceGuid},
-		Entity: cfclient.Space{OrgURL: "/v2/organizations/" + orgGuid},
-	}
-}
-
-func testOrgResource(orgGuid string) cfclient.OrgResource {
-	return cfclient.OrgResource{
-		Meta:   cfclient.Meta{Guid: orgGuid},
-		Entity: cfclient.Org{},
-	}
-}
-
-func testNewEvent(appGuid string) *events.Envelope {
-	metric := &events.ContainerMetric{
-		ApplicationId: proto.String(appGuid),
-		InstanceIndex: proto.Int32(1),
-		CpuPercentage: proto.Float64(2),
-		MemoryBytes:   proto.Uint64(3),
-		DiskBytes:     proto.Uint64(4),
-	}
-	event := &events.Envelope{
-		ContainerMetric: metric,
-		EventType:       events.Envelope_ContainerMetric.Enum(),
-		Origin:          proto.String("fake-origin-1"),
-		Timestamp:       proto.Int64(time.Now().UnixNano()),
-	}
-
-	return event
-}
-
-type testWebsocketHandler struct {
-	conns map[string]*websocket.Conn
-	sync.RWMutex
-}
-
-func (t *testWebsocketHandler) WriteMessage(appGuid string) error {
-	t.Lock()
-	defer t.Unlock()
-	Expect(t.conns).To(HaveKey(appGuid))
-	conn := t.conns[appGuid]
-	buf, _ := proto.Marshal(testNewEvent(appGuid))
-	return conn.WriteMessage(websocket.BinaryMessage, buf)
-}
-
-func (t *testWebsocketHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	re := regexp.MustCompile(`/apps/([^/]+)/stream`)
-	match := re.FindStringSubmatch(r.URL.Path)
-	Expect(match).To(HaveLen(2), "unable to extract app GUID from request path")
-	appGuid := match[1]
-
-	upgrader := websocket.Upgrader{
-		CheckOrigin: func(r *http.Request) bool { return true },
-	}
-
-	t.RLock()
-	Expect(t.conns).ToNot(HaveKey(appGuid))
-	t.RUnlock()
-
-	conn, err := upgrader.Upgrade(w, r, nil)
-	Expect(err).NotTo(HaveOccurred())
-	t.Lock()
-	t.conns[appGuid] = conn
-	t.Unlock()
-	defer conn.Close()
-
-	cm := websocket.FormatCloseMessage(websocket.CloseNormalClosure, "")
-	defer conn.WriteControl(websocket.CloseMessage, cm, time.Time{})
-
-	err = t.WriteMessage(appGuid)
-	Expect(err).ToNot(HaveOccurred())
-
-	for {
-		if _, _, err := conn.NextReader(); err != nil {
-			break
-		}
-	}
-}
