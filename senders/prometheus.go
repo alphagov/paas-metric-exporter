be2fc995b7d31cf2aee15de9432cbf61c361186a
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2018-10-19 10:13:44 +0100

    remove job label
    The "job" label isn't terribly informative to users, and it also
    clashes with the existing prometheus terminology around "job".  Let's
    remove it.

--- senders/prometheus.go
+++ senders/prometheus.go
@@ -140,7 +140,7 @@ func (s *PrometheusSender) labels(metric metrics.Metric, labelNames []string) pr
 
 	for mk, mv := range metric.GetLabels() {
 		switch mk {
-		case "GUID", "CellId":
+		case "GUID", "CellId", "Job":
 			continue
 		}
 		presented := s.presenter.Present(mk)
@@ -163,7 +163,7 @@ func (s *PrometheusSender) labels(metric metrics.Metric, labelNames []string) pr
 func (s *PrometheusSender) buildLabelsFromMetric(metric metrics.Metric) (labelNames []string) {
 	for k := range metric.GetLabels() {
 		switch k {
-		case "GUID", "CellId":
+		case "GUID", "CellId", "Job":
 			continue
 		}
 		presented := s.presenter.Present(k)
